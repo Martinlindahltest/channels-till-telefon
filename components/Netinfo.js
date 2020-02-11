ba3a1a6a8b5e15fcdb40689c2b3f0d1a1aa90f09
Martinlindahltest <basmartin@gmail.com>
2020-02-11 16:35:19 +0100

    tar emot data från irland

--- components/Netinfo.js
+++ components/Netinfo.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import NetInfo from '@react-native-community/netinfo';
+import Pusher from 'pusher-js/react-native';
+
 
 
 
@@ -9,6 +11,15 @@ const Netinfo = () => {
     NetInfo.fetch().then(state => {
         console.log('Connection type', state.type);
         console.log('Is connected?', state.isConnected);
+    });
+
+    const pusher = new Pusher("959108219678a5d6f07d", {
+        cluster: "eu",
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function (data) {
+        alert(JSON.stringify(data));
     });
 
     return (
